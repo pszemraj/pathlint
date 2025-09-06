0285f7ce6a4df65b92acb39fbfb26d9aeb9c5e96
Peter Szemraj <peterszemraj+dev@gmail.com>
2025-09-06 15:46:53 -0400

    Switch to automatic versioning from git tags
    - Use hatch-vcs for automatic version detection from git tags
    - Version is now dynamic, no need to manually update
    - Package version follows git tags (e.g., v1.0.0 -> version 1.0.0)
    - Development versions include commit hash and date

--- src/pathlint/__init__.py
+++ src/pathlint/__init__.py
@@ -1,3 +1,7 @@
 """Fast linter to detect os.path usage and encourage pathlib adoption."""
 
-__version__ = "1.0.0"
+try:
+    from ._version import __version__, __version_tuple__
+except ImportError:
+    __version__ = "0.0.0+unknown"
+    __version_tuple__ = (0, 0, 0, "+unknown")
